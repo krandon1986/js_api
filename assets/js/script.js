916210502ccd57334812cb62790269d960778079
krandon1986 <krandon1986@yahoo.co.uk>
2023-04-19 12:28:57 +0000

    Populate the javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
-25FBuNutt8kKDNIyfmPol8VamwQ
+const API_KEY = "25FBuNutt8kKDNIyfmPol8VamwQ";
+const API_URL ="https://ci-jshint.herokuapp.com/api";
+const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
